beabcb656e7f4386385d4bc4f9a5764b0f2b8c65
CaptCortez <georgisotirov88@gmail.com>
2014-01-06 14:23:48 +0200

    second commit test

--- AppChatService/Controllers/ValuesController.cs
+++ AppChatService/Controllers/ValuesController.cs
@@ -10,7 +10,7 @@ namespace AppChatService.Controllers
     public class ValuesController : ApiController
     {
         // GET api/values
-        public IEnumerable<string> Get()  
+        public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
         }
